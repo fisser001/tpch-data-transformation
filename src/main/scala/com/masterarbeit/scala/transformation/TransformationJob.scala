93d2048f5dc677a8f62c8052c9cf4f9c330339a5
Fisser <mfisser@sva.de>
2019-03-03 19:29:45 +0100

    initial commit

--- src/main/scala/com/masterarbeit/scala/transformation/TransformationJob.scala
+++ src/main/scala/com/masterarbeit/scala/transformation/TransformationJob.scala
@@ -1,7 +1,8 @@
 package com.masterarbeit.scala.transformation
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
-
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
 object TransformationJob {
 
@@ -14,22 +15,43 @@ object TransformationJob {
 
     import spark.implicits._
 
-    val customer_raw = "C:\\HFTL\\Masterarbeit\\DWH\\TPC_H\\tpch_sf1\\data_sf1\\customer.csv"
+    val customer_raw = "C:\\Daten\\Projekte\\Masterarbeit\\TPC_H\\tool\\2.17.3\\dbgen\\Debug\\tpch_sf1\\data_sf1\\customer.csv"
+    val customer_schema = StructType(Array(
+      StructField("c_custkey", StringType, true),
+      StructField("c_name", StringType, true),
+      StructField("c_address", StringType, true),
+      StructField("c_nationkey", StringType, true),
+      StructField("c_phone", StringType, true),
+      StructField("c_acctbal", DoubleType, true),
+      StructField("c_mktsegment", StringType, true),
+      StructField("c_comment", StringType, true),
+      StructField("last_col", StringType, true)))
 
-    val customer = spark.read.format("csv")
-      .option("header", "false")
-      .option("inferSchema", "true")
-      .option("delimiter", "|")
-      .load(customer_raw)
-      .drop($"_c8")
+    val region_raw = "C:\\Daten\\Projekte\\Masterarbeit\\TPC_H\\tool\\2.17.3\\dbgen\\Debug\\tpch_sf1\\data_sf1\\region.csv"
+    val region_schema = StructType(Array(
+      StructField("r_regionkey", StringType, true),
+      StructField("r_name", StringType, true),
+      StructField("r_comment", StringType, true),
+      StructField("last_col", StringType, true)))
 
-    customer.show(2, false)
-    println(customer.count)
+    val nation_raw = "C:\\Daten\\Projekte\\Masterarbeit\\TPC_H\\tool\\2.17.3\\dbgen\\Debug\\tpch_sf1\\data_sf1\\nation.csv"
+    val nation_schema = StructType(Array(
+      StructField("n_nationkey", StringType, true),
+      StructField("n_name", StringType, true),
+      StructField("n_regionkey", StringType, true),
+      StructField("n_comment", StringType, true),
+      StructField("last_col", StringType, true)))
 
-    customer.write.mode("Overwrite")
+    val customerDf = readData(spark,customer_raw,customer_schema)
+    val regionDf = readData(spark,region_raw,region_schema)
+    val nationDf = readData(spark,nation_raw,nation_schema)
+
+    val denormDf = denormJoin(spark, customerDf,nationDf, regionDf)
+
+    /*customer.write.mode("Overwrite")
       .format("orc")
       .option("compression", "zlib")
-      .save("C:/HFTL/Masterarbeit/DWH/TPC_H/tpch_sf1/data_sf1/customer")
+      .save("C:/HFTL/Masterarbeit/DWH/TPC_H/tpch_sf1/data_sf1/customer")*/
 
     val sourceDf:DataFrame = Seq(
       (
@@ -47,5 +69,32 @@ object TransformationJob {
 
    sourceDf.show(false)
   }
+
+  def readData(spark:SparkSession, dataPath: String,schemaOfCsv: StructType):DataFrame = {
+    import spark.implicits._
+    val data: DataFrame = spark.read.format("csv")
+      .option("header", "false")
+      .option("delimiter", "|")
+      .schema(schemaOfCsv)
+      .load(dataPath)
+      .drop($"last_col")
+
+    data.show(2, false)
+    println(data.count)
+    return data
+  }
+
+  def denormJoin(spark:SparkSession, customer: DataFrame,nation: DataFrame, region: DataFrame):DataFrame = {
+    import spark.implicits._
+
+    val joined_df = customer
+      .join(nation, col("c_nationkey") === col("n_nationkey"), "left")
+      .join(region, col("n_regionkey") === col("r_regionkey"), "left")
+
+    joined_df.show(2, false)
+    println(joined_df.count)
+    return joined_df
+  }
+
 
 }
